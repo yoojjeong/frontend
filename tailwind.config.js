0482c92389473f71faaa971f9c4f5bd9593aae25
Hyemin <star970909@gmail.com>
2025-03-12 10:28:28 +0900

    tailwindcss test

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // 리액트 프로젝트의 모든 js, jsx, ts, tsx 파일에 Tailwind 적용
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
