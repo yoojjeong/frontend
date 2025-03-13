659c2f19d6b661da172d0971d094ea7518f6e869
Hyemin <star970909@gmail.com>
2025-03-13 15:37:03 +0900

    time.js의 오류 수정

--- src/components/Time.js
+++ src/components/Time.js
@@ -14,7 +14,7 @@ export default function Time() {
   }, []);
 
   return (
-    <div className="text-white font-medium text-sm">
+    <div className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">
       <span>{`${dateTime.date} ${dateTime.time}`}</span>
     </div>
   );
