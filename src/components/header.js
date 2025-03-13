fe9858ae2867b8e717fcbfb5094a03b9071ff478
Hyemin <star970909@gmail.com>
2025-03-13 16:30:33 +0900

    weather.js - openweathermap api로 실시간 날씨를 보여주는 컴포넌트 생성

--- src/components/header.js
+++ src/components/header.js
@@ -1,4 +1,5 @@
 import Time from "./Time";
+import Weather from "./Weather";
 
 export default function Headers() {
   return (
@@ -19,8 +20,9 @@ export default function Headers() {
             <div className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">
               본점
             </div>
-            {/* 날짜/시간 넣을 곳 */}
+            {/* 날씨 + 날짜/시간 넣을 곳 */}
             <Time />
+            <Weather />
           </div>
         </div>
       </nav>
