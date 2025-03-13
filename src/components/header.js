2001727aacc7570cb2df7a9a3e0c5cdcaf14a906
Hyemin <star970909@gmail.com>
2025-03-13 12:24:26 +0900

    css수정 전 임시저장

--- src/components/header.js
+++ src/components/header.js
@@ -13,6 +13,8 @@ export default function Headers() {
     <header>
       <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
         <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
+          {/* 로고, 홈페이지 경로 -> 나중에 수정 */}
+          {/* a href는 나중에 Link로 변경 */}
           <a href="https://flowbite.com" class="flex items-center">
             <img
               src="https://flowbite.com/docs/images/logo.svg"
