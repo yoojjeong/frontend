54a33344fda9f20ad71f939f20a92c4b55a5618d
Hyemin <star970909@gmail.com>
2025-03-13 15:24:16 +0900

    header에 시간을 나타내는 Time 컴포넌트 생성

--- src/components/Time.js
+++ src/components/Time.js
@@ -1,5 +1,39 @@
 // 현재 시각을 출력하는 함수
 
+import { useEffect, useState } from "react";
+
 export default function Time() {
-  return <></>;
+  const [dateTime, setDateTime] = useState(getFormattedDateTime());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setDateTime(getFormattedDateTime());
+    }, 1000); // 1초마다 시간과 날짜 갱신
+
+    return () => clearInterval(interval); // 컴포넌트 언마운트 시 interval 정리
+  }, []);
+
+  return (
+    <div className="text-white font-medium text-sm">
+      <span>{`${dateTime.date} ${dateTime.time}`}</span>
+    </div>
+  );
+}
+
+// 날짜-시간 포매팅
+function getFormattedDateTime() {
+  const date = new Date();
+
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const day = String(date.getDate()).padStart(2, "0");
+
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  const seconds = String(date.getSeconds()).padStart(2, "0");
+
+  return {
+    date: `${year}-${month}-${day}`, // 2025-03-13
+    time: `${hours}:${minutes}:${seconds}`, // 15:21:00
+  };
 }
--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,6 @@
-import { useState } from "react";
+import Time from "./Time";
 
 export default function Headers() {
-  // 메뉴의 열고 닫는 상태를 관리
-  const [isOpen, setIsOpen] = useState(false);
-
-  // 메뉴를 열고 닫음 -> 이전 상태를 반전시키고 저장
-  const toggleMenu = () => {
-    setIsOpen((prev) => !prev);
-  };
-
   return (
     <header>
       <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
@@ -27,9 +19,8 @@ export default function Headers() {
             <div className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">
               본점
             </div>
-            <div className="text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800">
-              Get started
-            </div>
+            {/* 날짜/시간 넣을 곳 */}
+            <Time />
           </div>
         </div>
       </nav>
