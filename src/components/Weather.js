158011000b6b1f313c0b5040548f20e57192d0eb
Hyemin <star970909@gmail.com>
2025-03-17 10:20:33 +0900

    weather 온도 소수점 한 자리까지 반올림

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -46,7 +46,7 @@ export default function Weather() {
             className="w-6 h-6"
           />
           <span>
-            {weather.name} {Math.round(weather.main.temp)}℃
+            {weather.name} {parseFloat(weather.main.temp.toFixed(1))}℃
           </span>
         </div>
       ) : (
