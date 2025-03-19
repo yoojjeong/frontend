4fe0edb8036463d10d4bf223e15cbaf4db32ff59
Hyemin <star970909@gmail.com>
2025-03-19 16:42:34 +0900

    M-Update httpStatisticsService.js

--- src/features/statistics/api/httpStatisticsService.js
+++ src/features/statistics/api/httpStatisticsService.js
@@ -11,15 +11,26 @@ const instance = axios.create({
   },
 });
 
-const today = new Date();
-const salesDate = today.toISOString().split("T")[0];
-
-export async function fetchTodaySales() {
-  const response = await instance.get(`/salesHourly/${salesDate}`);
+// 일 단위의 시간대별 매출 데이터
+export async function fetchGetSales(date) {
+  const response = await instance.get(`/salesHourly/${date}`);
   return response;
 }
 
-export async function fetchGetSales({ date }) {
-  const response = await instance.get(`/salesHourly/${date}`);
+// 일 단위의 시간대별, 카테고리별 매출 데이터
+export async function fetchGetDaily(date) {
+  const response = await instance.get(`/salesDaily/${date}`);
+  return response;
+}
+
+// 월 단위의 시간대별, 카테고리별 매출 데이터
+export async function getchGetMontly(month) {
+  const response = await instance.get(`/salesMonthly/${month}`);
+  return response;
+}
+
+// 연 단위의 시간대별, 카테괼별 매출 데이터
+export async function fetchGetYearly(year) {
+  const response = await instance.get(`/salesYearly/${year}`);
   return response;
 }
