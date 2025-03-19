3cbcd6ae0ad19d50be4506835e8b75eebf7b6ecc
Hyemin <star970909@gmail.com>
2025-03-19 15:39:26 +0900

    Revert "Merge branch 'main' of https://github.com/KDT7team1/frontend"
    This reverts commit c922bcfce1c5fa2740485b3326654e38e31b1ff7, reversing
    changes made to b224ced7b6042c1eeabb23231010d410e4026e73.

--- src/Main.js
+++ src/Main.js
@@ -1,38 +1,71 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import RootLayout from "./pages/RootLayout";
+<<<<<<< HEAD
 import Home from "./pages/Home";
 import Statistics from "./features/statistics/pages/Statistics";
 import Login from './features/member/components/Login';
+=======
+import Association from "./features/cart_analysis/pages/Association";
+import GoodsList from "./features/cart_analysis/pages/GoodsList";
+import GoodsByCategory from "./features/cart_analysis/pages/GoodsByCategory";
+import GoodsBySubCategory from "./features/cart_analysis/pages/GoodsBySubCategory";
+import GoodsDetail from "./features/cart_analysis/pages/GoodDetail";
 
-const router = createBrowserRouter(
-  [
-    {
-      path: "/",
-      element: <RootLayout />,
-      id: "root",
-      children: [
-        {
-          index: true, // '/' 경로를 의미
-          element: <Home />,
-        },
-        {
-          path: "statistics", // 상대 경로로 설정
-          element: <Statistics />,
-        },
-        {
-          path: "app/member/login", // 상대 경로로 변경
-          element: <Login />, // 로그인 페이지 컴포넌트를 추가
-        },
-      ],
-    },
-  ],
+import InventoriesList from "./features/inventory/pages/InventoriesList";
+import SalesToday from "./features/statistics/pages/SalesToday";
+import DashBoard from "./pages/DashBoard";
+>>>>>>> parent of c922bcf (Merge branch 'main' of https://github.com/KDT7team1/frontend)
+
+const router = createBrowserRouter([
   {
-    future: {
-      v7_relativeSplatPath: true, // v7 방식으로 경로 해석
-    },
-  }
-);
+    path: "/",
+    element: <RootLayout />,
+    id: "root",
+    children: [
+      {
+        // 메인 화면
+        path: "/",
+        element: <DashBoard />,
+      },
+      {
+        // 매출 조회
+        path: "/statistics/salesToday",
+        element: <SalesToday />,
+      },
+      {
+        // 상품찾기
+        path: "/categories/findAll",
+        element: <GoodsList />,
+      },
+      {
+        // 카테고리별
+        path: "/categories/:firstname",
+        element: <GoodsByCategory />,
+      },
+      {
+        // 카테고리별 ( 소분류 )
+        path: "/categories/:firstname/:secondname",
+        element: <GoodsBySubCategory />,
+      },
+      {
+        // 상품상세보기
+        path: "/goods/findById/:id",
+        element: <GoodsDetail />,
+      },
+      {
+        // 장바구니분석
+        path: "/association",
+        element: <Association />,
+      },
+      {
+        // 재고관리 (전체재고조회)
+        path: "/inventory/findAll",
+        element: <InventoriesList />,
+      },
+    ],
+  },
+]);
 
 export default function Main() {
   return <RouterProvider router={router} />;
