88f4ac17ac96e992ed901838e3309532bdba2ae8
Hyemin <star970909@gmail.com>
2025-03-19 15:27:28 +0900

    tmp

--- src/features/statistics/pages/Statistics.js
+++ src/features/statistics/pages/Statistics.js
@@ -25,23 +25,23 @@ export default function Statistics() {
 
   // 최초 렌더링 시 오늘 날짜를 설정
 
-  // 캘린더에서 날짜를 선택했을 때 쿼리 스트링을 업데이트
-  useEffect(() => {
-    // 초기값
-    const params = new URLSearchParams(search);
-    const dateFromQuery = params.get("date");
+  // // 캘린더에서 날짜를 선택했을 때 쿼리 스트링을 업데이트
+  // useEffect(() => {
+  //   // 초기값
+  //   const params = new URLSearchParams(search);
+  //   const dateFromQuery = params.get("date");
 
-    // 쿼리스트링이 비어 있으면 오늘 날짜로 설정
-    const date = dateFromQuery ? new Date(dateFromQuery) : new Date();
+  //   // 쿼리스트링이 비어 있으면 오늘 날짜로 설정
+  //   const date = dateFromQuery ? new Date(dateFromQuery) : new Date();
 
-    setSelectedDate(date);
-    setFormattedDate(date.toISOString().split("T")[0]);
+  //   setSelectedDate(date);
+  //   setFormattedDate(date.toISOString().split("T")[0]);
 
-    // 쿼리스트링 업데이트
-    const query = new URLSearchParams(window.location.search);
-    query.set("date", formattedDate);
-    navigate(`/statistics?${query.toString()}`);
-  }, []);
+  //   // 쿼리스트링 업데이트
+  //   const query = new URLSearchParams(window.location.search);
+  //   query.set("date", formattedDate);
+  //   navigate(`/statistics?${query.toString()}`);
+  // }, []);
 
   return (
     <div className="flex flex-col gap-4">
